6fa7ef4b092c1d69dc0ceee8641a8807f1c82ccd
vigneshdayalan <vikiguru18@gmail.com>
2024-05-30 22:17:31 +0100

    ADD: simple calculator project and unit tests

---
+++ SimpleCalculator.Tests/CalculatorTests.cs
@@ -0,0 +1,45 @@
+using SimpleCalculator;
+using Xunit;
+
+namespace SimpleCalculator.Tests
+{
+    public class CalculatorTests
+    {
+        private readonly Calculator _calculator;
+
+        public CalculatorTests()
+        {
+            _calculator = new Calculator();
+        }
+
+        [Fact]
+        public void Add_ReturnsCorrectSum()
+        {
+            Assert.Equal(8, _calculator.Add(5, 3));
+        }
+
+        [Fact]
+        public void Subtract_ReturnsCorrectDifference()
+        {
+            Assert.Equal(2, _calculator.Subtract(5, 3));
+        }
+
+        [Fact]
+        public void Multiply_ReturnsCorrectProduct()
+        {
+            Assert.Equal(15, _calculator.Multiply(5, 3));
+        }
+
+        [Fact]
+        public void Divide_ReturnsCorrectQuotient()
+        {
+            Assert.Equal(5.0 / 3, _calculator.Divide(5, 3), 5);
+        }
+
+        [Fact]
+        public void Divide_ByZero_ThrowsDivideByZeroException()
+        {
+            Assert.Throws<DivideByZeroException>(() => _calculator.Divide(5, 0));
+        }
+    }
+}
---
+++ SimpleCalculator/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace SimpleCalculator
+{
+    public class Calculator
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+
+        public int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+
+        public double Divide(int a, int b)
+        {
+            if (b == 0)
+            {
+                throw new DivideByZeroException("Division by zero is not allowed.");
+            }
+            return (double)a / b;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator calculator = new Calculator();
+            Console.WriteLine("Addition: " + calculator.Add(5, 3));
+            Console.WriteLine("Subtraction: " + calculator.Subtract(5, 3));
+            Console.WriteLine("Multiplication: " + calculator.Multiply(5, 3));
+            Console.WriteLine("Division: " + calculator.Divide(5, 3));
+        }
+    }
+}
